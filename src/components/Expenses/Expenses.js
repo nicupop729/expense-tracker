1fc62f377f3d0be12bc81015f38210ced673edfb
Nicolae Pop <nicupop729@gmail.com>
2022-01-03 17:14:33 -0300

    feat: build Expenses component

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+const Expenses = ({ items }) => {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={items[0].title}
+        amount={items[0].amount}
+        date={items[0].date}
+      />
+      <ExpenseItem
+        title={items[1].title}
+        amount={items[1].amount}
+        date={items[1].date}
+      />
+      <ExpenseItem
+        title={items[2].title}
+        amount={items[2].amount}
+        date={items[2].date}
+      />
+      <ExpenseItem
+        title={items[3].title}
+        amount={items[3].amount}
+        date={items[3].date}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
