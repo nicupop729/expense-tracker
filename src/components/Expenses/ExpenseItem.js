dfa3283e9b89b3f3dc1c169b4898501302e6968c
Nicolae Pop <nicupop729@gmail.com>
2022-01-03 18:47:10 -0300

    feat: filter expenses by year

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  
-  const clickHandler = () => {
-    setTitle('Updated!');
-  };
-
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">£{props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
